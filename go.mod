8b20ed3b5a9c3fde2c4b0c843eda4f4b9fd6e056
meridianss <meridianss@WhiteThunder>
2023-09-16 10:47:38 +0200

    added dog as a dependency and new BigBark, BigBarks functions

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/tsutheone/puppy
 
 go 1.21.1
+
+require github.com/tsutheone/dog v0.0.0-20230916084133-158b0eb9390c // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+	"github.com/tsutheone/dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBark() string {
+	return dog.DoggoGrowsUp(Bark())
+}
+
+func BigBarks() string {
+	return dog.DoggoGrowsUp(Barks())
 }
